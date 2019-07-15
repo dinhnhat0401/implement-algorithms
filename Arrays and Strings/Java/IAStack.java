989bdd01edb85753a39a313c46902d270b8e26a7
dinhnhat0401 <nhatphutho@gmail.com>
2019-07-15 16:18:05 +0900

    [Java] stack: handle edge case when top == null

--- Arrays and Strings/Java/IAStack.java
+++ Arrays and Strings/Java/IAStack.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 public class IAStack<T> {
   private class StackNode<T> {
     T data;
@@ -12,10 +14,13 @@ public class IAStack<T> {
   private StackNode<T> top;
   
   public T peek() {
+    if (top == null) throw new EmptyStackException();
     return top.data;
   }
 
   public T pop() {
+    if (top == null) throw new EmptyStackException();
+
     T data = top.data;
     top = top.next;
 
@@ -28,16 +33,23 @@ public class IAStack<T> {
     top = node;
   }
 
+  boolean isEmpty() {
+    return top == null;
+  }
 
   public static void main(String [] args) {
     IAStack<Integer> stack = new IAStack<>();
+    assert(stack.isEmpty() == true): "A newly created stack must be empty";
     stack.push(1);
+    assert(stack.isEmpty() == false): "A stack shouldn't be empty after being added a node";
     stack.push(2);
     stack.push(3);
     assert(stack.peek() == 3): "A stack must be a LIFO data structure";
-    assert(stack.peek() == 3): "A peek method shouldn't remove any node from Stack";
-    assert(stack.pop() == 3): "A pop method should return current top and remove it from current stack";
-    assert(stack.peek() == 2): "A stack must be a LIFO data structure";
+    assert(stack.peek() == 3): "A peek method shouldn't remove any node from a Stack";
+    stack.pop();
+    stack.pop();
+    assert(stack.pop() == 1): "A pop method should remove top from a Stack and return that element";
+    assert(stack.isEmpty() == true): "After removing all elements from a Stack, it should be empty";
   }
 }
 
