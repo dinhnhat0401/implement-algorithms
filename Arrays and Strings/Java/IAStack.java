4c6c9c66e33a64e88d874e1590636a655b906bcd
dinhnhat0401 <nhatphutho@gmail.com>
2019-07-15 19:43:14 +0900

    [Java] min stack

--- Arrays and Strings/Java/IAStack.java
+++ Arrays and Strings/Java/IAStack.java
@@ -1,9 +1,11 @@
+import java.util.Comparator;
 import java.util.EmptyStackException;
 
-public class IAStack<T> {
+public class IAStack<T extends Comparable<T>> {
   private class StackNode<T> {
     T data;
     StackNode<T> next;
+    T min;
   
     StackNode(T data) {
       this.data = data;
@@ -29,12 +31,24 @@ public class IAStack<T> {
 
   void push(T data) {
     StackNode<T> node = new StackNode<>(data);
+
+    if (top == null || data.compareTo(top.min) < 0) {
+      node.min = data;
+    } else {
+      node.min = top.min;
+    }
+
     node.next = top;
     top = node;
   }
 
   boolean isEmpty() {
     return top == null;
+  }
+
+  public T min() {
+    if (top == null) throw new EmptyStackException();
+    return top.min;
   }
 
   public static void main(String [] args) {
@@ -44,11 +58,15 @@ public class IAStack<T> {
     assert(stack.isEmpty() == false): "A stack shouldn't be empty after being added a node";
     stack.push(2);
     stack.push(3);
+    assert(stack.min() == 1): "Stack min value is wrong. Check it";
     assert(stack.peek() == 3): "A stack must be a LIFO data structure";
     assert(stack.peek() == 3): "A peek method shouldn't remove any node from a Stack";
+    stack.push(0);
+    assert(stack.min() == 0): "Stack min value is wrong. Check it";
     stack.pop();
     stack.pop();
-    assert(stack.pop() == 1): "A pop method should remove top from a Stack and return that element";
+    stack.pop();
+    stack.pop();
     assert(stack.isEmpty() == true): "After removing all elements from a Stack, it should be empty";
   }
 }
