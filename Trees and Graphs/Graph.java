dba4380f37c3a1b486dcf8fd83b9a0530bb36612
dinhnhat0401 <nhatphutho@gmail.com>
2019-07-19 23:24:12 +0900

    [java] build graph from a text file contains edges info

--- Trees and Graphs/Graph.java
+++ Trees and Graphs/Graph.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Deque;
 import java.util.List;
@@ -5,7 +10,47 @@ import java.util.LinkedList;
 import java.util.Set;
 
 public class Graph {
-  List<Node> nodes;
+  int size;
+  List<Node> nodes = new ArrayList<>();
+
+  public Graph(final Path path) {
+    try (final BufferedReader br = Files.newBufferedReader(path, Charset.forName("UTF-8"));) {
+      boolean firstLine = true;
+      String data;
+      while ((data = br.readLine()) != null) {
+        if (firstLine) {
+          this.size = Integer.parseInt(data);
+          for (int i = 0; i < this.size; i++) {
+            Node node = new Node(i);
+            this.nodes.add(node);
+          }
+          firstLine = false;
+          continue;
+        }
+
+        String[] line = data.trim().split(" ");
+        int u = Integer.parseInt(line[0]);
+        int v = Integer.parseInt(line[1]);
+        addEdge(u, v);
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  private void addEdge(final int u, final int v) {
+    this.nodes.get(u).getAdjacent().add(this.nodes.get(v));
+  }
+
+  public void printGraph() {
+    this.nodes.stream().forEach((node) -> {
+      System.out.print(node.name + " -> ");
+      node.getAdjacent().stream().forEach((adjNode) -> {
+        System.out.print(adjNode.name + ",");
+      });
+      System.out.print("\n");
+    });
+  }
 
   // BFS
   boolean routeBetweenNodes(Node node1, Node node2) {
@@ -28,15 +73,22 @@ public class Graph {
     }
 
     return false; // Not found, searched whole graph though 
+  }
+
+  public static void main(String [] args) {
+     final Path inputPath = Paths.get(args[0]);
+     Graph g = new Graph(inputPath);
+     g.printGraph();
   } 
 }
 
 class Node {
-  private String name;
-  private List<Node> adjacent;
+  int name;
+  List<Node> adjacent;
 
-  public Node(String name) {
+  public Node(int name) {
     this.name = name;
+    adjacent = new ArrayList<>();
   }
 
   public void setAdjacent(List<Node> adjacent) {
