601efd72facc9d821672874c07603a51c145ebca
dinhnhat0401 <nhatphutho@gmail.com>
2019-11-16 23:01:20 +0900

    data structure project

---
+++ Data structures/Swift/DataStructure/DataStructure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataStructure
+//
+//  Created by Đinh Văn Nhật on 2019/11/16.
+//  Copyright © 2019 Đinh Văn Nhật. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
