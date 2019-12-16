327ba1169d02a58769f2c1312c696f92b19267c2
dinhnhat0401 <nhatphutho@gmail.com>
2019-12-16 22:53:14 +0900

    Leetcode 1293 running with TLE

--- Data structures/Swift/DataStructure/Leetcode/ShortestPathInAGrid.swift
+++ Data structures/Swift/DataStructure/Leetcode/ShortestPathInAGrid.swift
@@ -9,39 +9,36 @@
 
 import Foundation
 class ShortestPathInAGrid {
+    private let INF = 9999
+
     func shortestPath(_ grid: [[Int]], _ k: Int) -> Int {
-        var memo = [[Int]](repeating: [Int](repeating: 9999, count: grid[0].count), count: grid.count)
-        return bfs(grid, 0, 0, k, 0, &memo)
+        let result = bfs(grid, 0, 0, k, 0)
+        return result == INF ? -1 : result
     }
 
-    func bfs(_ grid: [[Int]], _ r: Int, _ c: Int, _ remain: Int, _ step: Int, _ memo: inout [[Int]]) -> Int {
+    func bfs(_ grid: [[Int]], _ r: Int, _ c: Int, _ remain: Int, _ step: Int) -> Int {
         var grid = grid
-        if r < 0 || c < 0 || r >= grid.count || c >= grid[0].count
-            || remain < 0 || memo[r][c] < step || (grid[r][c] == 1 && remain < 1) {
-            return 9999
+        if r < 0 || c < 0 || r >= grid.count || c >= grid[0].count || remain < 0 || grid[r][c] == 2 || (grid[r][c] == 1 && remain < 1) {
+            return INF
         }
 
         if r == grid.count - 1 && c == grid[0].count - 1 {
-            memo[r][c] = step
             return step
         }
 
         let nextRemain = remain - (grid[r][c] == 1 ? 1 : 0)
+        grid[r][c] = 2
 
-        let left = bfs(grid, r - 1, c, nextRemain, step + 1, &memo)
-        let right = bfs(grid, r + 1, c, nextRemain, step + 1, &memo)
-        let up = bfs(grid, r, c - 1, nextRemain, step + 1, &memo)
-        let down = bfs(grid, r, c + 1, nextRemain, step + 1, &memo)
+        let left = bfs(grid, r - 1, c, nextRemain, step + 1)
+        let right = bfs(grid, r + 1, c, nextRemain, step + 1)
+        let up = bfs(grid, r, c - 1, nextRemain, step + 1)
+        let down = bfs(grid, r, c + 1, nextRemain, step + 1)
 
-        var result = 9999
+        var result = INF
         result = min(result, left)
         result = min(result, right)
         result = min(result, up)
         result = min(result, down)
-
-        if result == 9999 {
-            return -1
-        }
 
         return result
     }
