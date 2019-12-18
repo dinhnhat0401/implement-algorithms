c8ceccc540099a5485e92a846905ee078b91a360
dinhnhat0401 <nhatphutho@gmail.com>
2019-12-18 22:39:56 +0900

    Shortest path in a grid 3 with Manhattan trick

--- Data structures/Swift/DataStructure/Leetcode/ShortestPathInAGrid.swift
+++ Data structures/Swift/DataStructure/Leetcode/ShortestPathInAGrid.swift
@@ -117,4 +117,81 @@ class ShortestPathInAGrid {
 
         return -1
     }
+
+    func shortestPath3(_ grid: [[Int]], _ k: Int) -> Int {
+        // idea:
+        // BFS with each element has form: [row, column, number of eliminated obstacles, step]
+        // also have a visited array to check if we visited the cell
+        // -> [row, column, number of eliminated obstacles] -> Bool
+        // enqueue first element [0, 0, k, 0]
+        // while queue is not empty
+            // dequeue get element e [r, c, re, step]
+            // for direction in [[-1, 0], [1, 0], [0, - 1], [0, 1]]
+                // enqueue if index1 in range [0, m), index2 in range [0, n)
+                // mark cell with remain step as visited
+                // end if we reached [m - 1, n - 1], return step
+
+        // return -1
+        let m = grid.count
+        let n = grid[0].count
+
+        if k >= m + n - 2 {
+            return m + n - 2
+        }
+
+        var visited = Set<[Int]>()
+        visited.insert([0, 0, k])
+        var frontier = [[0, 0, k]]
+        var level = 0
+
+        while frontier.count > 0 {
+            var next = [[Int]]()
+            for e in frontier {
+                let r = e[0]
+                let c = e[1]
+                let remain = e[2]
+
+                // return right after we reach [m-1, n-1] cell
+                if remain >= m + n - 3 - r - c {
+                    return level + m + n - 2 - r - c
+                }
+
+                for d in [[-1, 0], [1, 0], [0, -1], [0, 1]] {
+                    let newRow = r + d[0]
+                    let newCol = c + d[1]
+
+                    if newRow < 0 || newRow >= m || newCol < 0 || newCol >= n {
+                        continue
+                    }
+
+                    var newRemain = remain - (grid[newRow][newCol] == 1 ? 1 : 0)
+                    if newRemain < 0 {
+                        continue
+                    }
+
+                    if visited.contains([newRow, newCol, newRemain]) {
+                        continue
+                    }
+
+                    // return right after we reach the cell that we know we can eliminate all obstacles if available on our way to [m-1, n-1]
+                    // This's called Manhattan trick
+                    // check this content:
+                    // https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/discuss/453652/Manhattan-distance-instead-of-normal-goal-check
+                    if newRemain >= m + n - 3 - newRow - newCol {
+                        return level + 1 + m + n - 2 - newRow - newCol
+                    }
+
+                    next.append([newRow, newCol, newRemain])
+                    // mark current cell as visited
+                    // it's important to mark [newRow, newCol, newRemain] as visited so we can avoid many repeated work with other cells in the same frontier
+                    visited.insert([newRow, newCol, newRemain])
+                }
+            }
+
+            level += 1
+            frontier = next
+        }
+
+        return -1
+    }
 }
