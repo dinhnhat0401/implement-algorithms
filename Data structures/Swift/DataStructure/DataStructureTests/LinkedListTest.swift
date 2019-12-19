67a7ea75e4ec60e580f5749d5978755e9c41e6f4
dinhnhat0401 <nhatphutho@gmail.com>
2019-12-19 23:30:00 +0900

    LinkedList

--- Data structures/Swift/DataStructure/DataStructureTests/LinkedListTest.swift
+++ Data structures/Swift/DataStructure/DataStructureTests/LinkedListTest.swift
@@ -10,7 +10,34 @@ import XCTest
 @testable import DataStructure
 
 class LinkedListTest: XCTestCase {
-    override class func setUp() {
-        
+    func testInsertOneElement() {
+        var linkedList = LinkedList<Int>()
+        linkedList.append(data: 1)
+        XCTAssertEqual(1, try linkedList.first())
+        XCTAssertEqual(1, try linkedList.last())
+        XCTAssertEqual(1, try linkedList.removeFirst())
+        XCTAssertEqual(true, linkedList.isEmpty())
+    }
+
+    func testEmptyLinkedList() {
+        let linkedList = LinkedList<Int>()
+        do {
+            print(try linkedList.first())
+        } catch {
+            let e = error as? LinkedListError
+            XCTAssertEqual(e, LinkedListError.EmptyLinkedList)
+        }
+    }
+
+    func testInsertMorethanOneElement() {
+        var linkedList = LinkedList<Int>()
+        linkedList.append(data: 1)
+        linkedList.append(data: 2)
+        linkedList.append(data: 3)
+        XCTAssertEqual(1, try linkedList.first())
+        XCTAssertEqual(3, try linkedList.last())
+        XCTAssertEqual(1, try linkedList.removeFirst())
+        XCTAssertEqual(2, try linkedList.first())
+        XCTAssertEqual(false, linkedList.isEmpty())
     }
 }
