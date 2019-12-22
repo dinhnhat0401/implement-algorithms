43b7f0eed2156db3132371ee13176a0ba2c15f3d
dinhnhat0401 <nhatphutho@gmail.com>
2019-12-23 00:29:26 +0900

    Dijkstra pseudo code

---
+++ Data structures/Swift/DataStructure/DataStructure/Graph/BellmanFord.swift
@@ -0,0 +1,9 @@
+//
+//  BellmanFord.swift
+//  DataStructure
+//
+//  Created by Đinh Văn Nhật on 2019/12/22.
+//  Copyright © 2019 Đinh Văn Nhật. All rights reserved.
+//
+
+import Foundation
---
+++ Data structures/Swift/DataStructure/DataStructure/Graph/Dijkstra.swift
@@ -0,0 +1,22 @@
+
+//
+//  Dijkstra.swift
+//  DataStructure
+//
+//  Created by Đinh Văn Nhật on 2019/12/22.
+//  Copyright © 2019 Đinh Văn Nhật. All rights reserved.
+//
+
+import Foundation
+
+class Dijkstra {
+//    Dijkstra (G, W, s) //uses priority queue Q
+//        Initialize (G, s)
+//        S ← φ
+//        Q ← V [G] //Insert into Q
+//        while Q = φ
+//            do u ← EXTRACT-MIN(Q) //deletes u from Q
+//            S = S ∪ {u}
+//            for each vertex v  Adj[u]
+//                do RELAX (u, v, w) ← this is an implicit DECREASE KEY operation
+}
