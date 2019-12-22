d77193ca2f2be7f0d08f5a90083068374452f99f
dinhnhat0401 <nhatphutho@gmail.com>
2019-12-23 01:02:01 +0900

    BellmanFord pseudo code

--- Data structures/Swift/DataStructure/DataStructure/Graph/BellmanFord.swift
+++ Data structures/Swift/DataStructure/DataStructure/Graph/BellmanFord.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+//Bellman-Ford(G,W,s)
+//    Initialize ()
+//    for i = 1 to |V | − 1
+//        for each edge (u, v) ∈ E:
+//            Relax(u, v)
+//
+//    // check
+//    for each edge (u, v) ∈ E
+//        do if d[v] > d[u] + w(u, v)
+//            then report a negative-weight cycle exists
